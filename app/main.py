6e726ab843fd4325f607f30a6f97a642ae9325ce
rkdgusdnr99 <112935900+rkdgusdnr99@users.noreply.github.com>
2026-04-06 16:33:05 +0900

    주석 제거, README 수정

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ def insert_user(count=1000):
     
     print(f"{count}개 데이터 삽입 시작 (executemany)...")
     cur.executemany(query, data)
-    print("데이터 삽입 완료!") # 👈 위 줄과 시작 위치를 똑같이 맞췄습니다.
+    print("데이터 삽입 완료!")
 
     conn.commit()
     cur.close()
@@ -33,18 +33,16 @@ def insert_log(count=1000000):
         print("유저가 없습니다! 먼저 유저부터 생성하세요.")
         return
     
-    # 2. 로그 생성 (리스트 안에 튜플 형태로 담아야 함)
+    # 2. 로그 생성
     logs = []
     for _ in range(count):
         log = generate_log(existing_ids)
-        # 중요: execute_values는 [(val1, val2), (val3, val4)] 형태를 원합니다.
         logs.append((log['user_id'], log['page']))
 
     # 3. DB 연결 및 execute_values 실행
     conn = get_connection()
     cur = conn.cursor()
     
-    # 주의: VALUES 뒤에 %s에 괄호를 붙이지 않습니다! (%s) -> %s
     query = "INSERT INTO logs (user_id, page) VALUES %s"
     
     print(f"{count}개 로그 삽입 시작...")
